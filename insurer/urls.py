189c7641aec1c53cb558242f701f57771b332b9e
Abdulhakeem Adetunji Mustapha <abdulhakeemmustapha@gmail.com>
2018-12-01 06:45:23 +0100

    fix url endpoint

--- insurer/urls.py
+++ insurer/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from .views import ListRiskView, RiskDetailView
 
 urlpatterns = [
-    url('^risk/$', ListRiskView.as_view()),
-    url('^risk/(?P<pk>[0-9]+)/$', RiskDetailView.as_view())
+    url('^risks/$', ListRiskView.as_view()),
+    url('^risks/(?P<pk>[0-9]+)/$', RiskDetailView.as_view())
 ]
