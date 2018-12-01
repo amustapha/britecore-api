d9683881f6982d28826ac4547c1517aa373f91e2
Abdulhakeem Adetunji Mustapha <abdulhakeemmustapha@gmail.com>
2018-12-01 15:04:23 +0100

    process form submission

--- insurer/admin.py
+++ insurer/admin.py
@@ -3,8 +3,8 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from .models import Field, Risk
+from .models import Field, Risk, SubmissionSet, SubmissionValue
 
 # Register your models here.
-admin.site.register(Field)
-admin.site.register(Risk)
+for model in [Field, Risk, SubmissionValue, SubmissionSet]:
+    admin.site.register(model)
