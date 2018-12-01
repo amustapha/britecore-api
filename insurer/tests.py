100c856eb447b516e5aae1df73278a66c0c9f818
Abdulhakeem Adetunji Mustapha <abdulhakeemmustapha@gmail.com>
2018-12-01 16:43:54 +0100

    Added test cases

--- insurer/tests.py
+++ insurer/tests.py
@@ -1,6 +1,22 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.urls import reverse
 from django.test import TestCase
+from rest_framework.test import APIRequestFactory, APITestCase
 
-# Create your tests here.
+
+class RiskTests(APITestCase):
+
+    def create_risk(self):
+        data = {"name":"Test insurance","description":"Test insurance risks","field_set":[{"field":"Name"},{"field":"Email", "type": "email"}]}
+        response = self.client.post(reverse('risk_list_or_create'), data)
+        self.assertEqual(response.status_code, 201)
+
+    def list_risks(self):
+        response = self.client.get(reverse('risk_list_or_create'))
+        self.assertEqual(response.status_code, 200)
+
+    def view_risk(self):
+        response = self.client.get(reverse('risk_detail', 1))
+        self.assertEqual(response.status_code, 201)
