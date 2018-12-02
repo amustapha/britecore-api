72e17580595d39b9d1cc663ed700ba558153dcb0
Abdulhakeem Adetunji Mustapha <abdulhakeemmustapha@gmail.com>
2018-12-02 20:29:16 +0100

    Added View submitted data

--- insurer/urls.py
+++ insurer/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 
-from .views import ListRiskView, RiskDetailView, SubmissionView
+from .views import ListRiskView, RiskDetailView, SubmissionView, ListSubmission
 
 urlpatterns = [
     url('^risks/$', ListRiskView.as_view(), name='risk_list_or_create'),
     url('^risks/(?P<pk>[0-9]+)/$', RiskDetailView.as_view(), name='risk_detail'),
-    url('^submit/$', SubmissionView.as_view(), name='risk_form_submission')
+    url('^submit/$', SubmissionView.as_view(), name='risk_form_submission'),
+    url('^submissions/(?P<risk>[0-9]+)/$', ListSubmission.as_view(), name='risk_form_submissions'),
 ]
